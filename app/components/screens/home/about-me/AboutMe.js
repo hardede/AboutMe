c15819f6d2416b65e1b423b3e712531d3af11b3d
hardede <dimakostyuk50@gmail.com>
2022-06-06 14:30:44 +0300

    fix

--- app/components/screens/home/about-me/AboutMe.js
+++ app/components/screens/home/about-me/AboutMe.js
@@ -12,7 +12,7 @@ const AboutMe = ({ me }) => {
     <div className={styles.me}>
       <div className={styles["image-wrapper"]}>
         <Image
-          src={`${APP_URL}/icons/avatar`}
+          src={`${APP_URL}/icons/avatar.jpg`}
           width={190}
           height={190}
           className={styles.image}
